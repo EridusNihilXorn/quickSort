79022c91e0bc7a481bfcee8364f77a5db1bb9ba9
NihilSherrKhaine <eridusnihilxorn@gmail.com>
2017-10-10 22:54:20 +0300

    Source code uploaded.
    Quicksort for simple template arrays, where T can use compare operators, not including STL arrays.

---
+++ qSort.cpp
@@ -0,0 +1,59 @@
+// qSort.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+template <class T>
+void quickSort(T* summable, int left, int right, int size)
+{
+	// Shuffle elements of array to maximize effectiveness for bad cases.
+	if (left < 0 || right >= size)
+	{
+		std::cout << "Wrong limits of array elements.\n";
+		return;
+	} 
+	if (left >= right) return;
+
+	int partitionEl = partition(summable, left, right);
+	quickSort(summable, left, partitionEl - 1, size);
+	quickSort(summable, partitionEl + 1, right, size);
+
+}
+template <class T>
+int partition(T* summable, int left, int right)
+{
+	int i = left, j = right + 1;
+	while (true)
+	{
+		while (summable[++i] < summable[left])	// Find element from left that does not fit it's position.
+			if (i == right) break;
+		while (summable[--j] > summable[left])	// Find element from right that ...
+			if (j == left) break;
+
+		if (i >= j) break;  // If pointers cross, no more swaps required.
+
+		exchange(summable, i, j);
+	}
+	exchange(summable, left, j);
+	return j;
+}
+
+template <class T>
+void exchange(T* summable, int i, int j)
+{
+	T buffer;
+	buffer = summable[i];
+	summable[i] = summable[j];
+	summable[j] = buffer;
+}
+
+//int main()
+//{
+//	int array1[10] = {7, 32, -11, 0, -29, -50, 38, 9, 28, -20};
+//	quickSort(array1, 0, 9,10);
+//	for (int i = 0; i < 10; i++)
+//		std::cout << array1[i] << " ";
+//    return 0;
+//}
+
